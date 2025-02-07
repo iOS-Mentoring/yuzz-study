d71854721557681bbd264e08aabfb73f354a397f
yujin <ydbwlsjki60@gmail.com>
2025-02-08 08:18:18 +0900

    [Feat] UIExtension Font 세팅

--- DailyTyping/DailyTyping/Sources/Extensions/UIExtension/UIFont+named.swift
+++ DailyTyping/DailyTyping/Sources/Extensions/UIExtension/UIFont+named.swift
@@ -17,3 +17,32 @@ public extension UIFont {
         type.font(size: size)
     }
 }
+
+public enum YDFontFamily {
+    
+    public enum PretendardType: String {
+        case Black
+        case Bold
+        case ExtraBold
+        case ExtraLight
+        case Light
+        case Medium
+        case Regular
+        case SemiBold
+        case Thin
+        
+        func font(size: CGFloat) -> UIFont {
+            return UIFont(name: "Pretendard-\(rawValue)", size: size) ?? .systemFont(ofSize: size, weight: .regular)
+        }
+    }
+    
+    public enum NanumMyeongjoType: String {
+        case Regular = ""
+        case Bold
+        case ExtraBold
+        
+        func font(size: CGFloat) -> UIFont {
+            return UIFont(name: "NanumMyeongjo\(rawValue)", size: size) ?? .systemFont(ofSize: size, weight: .regular)
+        }
+    }
+}
