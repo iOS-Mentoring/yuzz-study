7372b330c4fa110aeedf7b853432189ab982d17f
yujin <ydbwlsjki60@gmail.com>
2025-02-08 07:48:13 +0900

    [Feat] BaseViewController 생성

---
+++ DailyTyping/DailyTyping/Sources/Base/BaseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseViewController.swift
+//  DailyTyping
+//
+//  Created by 조유진 on 2/8/25.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
