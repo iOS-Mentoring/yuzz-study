bf040ecd1e627b15bc04f55d50bfb10a95c763fe
yujin <ydbwlsjki60@gmail.com>
2025-02-08 07:31:06 +0900

    [Feat] 프로젝트 생성

---
+++ DailyTyping/DailyTyping/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyTyping
+//
+//  Created by 조유진 on 2/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
