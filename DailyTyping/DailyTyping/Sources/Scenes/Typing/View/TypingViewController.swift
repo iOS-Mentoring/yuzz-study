c0cbc419679069ce015031aae19aefbdf2b1e337
yujin <ydbwlsjki60@gmail.com>
2025-02-08 07:51:14 +0900

    [Feat] TypingViewController 생성

---
+++ DailyTyping/DailyTyping/Sources/Scenes/Typing/View/TypingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TypingViewController.swift
+//  DailyTyping
+//
+//  Created by 조유진 on 2/8/25.
+//
+
+import UIKit
+
+class TypingViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
