b41b944de391be9a9b34653e5556f2be6a28b992
yuzzin <ydbwlsjki60@gmail.com>
2025-02-11 16:47:25 +0900

    [Feat] BaseViewController combine 관련 세팅

--- DailyTyping/DailyTyping/Sources/Base/BaseViewController.swift
+++ DailyTyping/DailyTyping/Sources/Base/BaseViewController.swift
@@ -6,15 +6,18 @@
 //
 
 import UIKit
+import Combine
 
 class BaseViewController: UIViewController {
+    var cancellables = Set<AnyCancellable>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavigationItem()
+        bind()
     }
+    
+    func bind() { }
 
-    func configureNavigationItem() {
-        
-    }
+    func configureNavigationItem() { }
 }
