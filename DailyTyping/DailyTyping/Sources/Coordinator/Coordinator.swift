bf6e147b126b38567b8197458b545847b55f7cf7
yuzzin <ydbwlsjki60@gmail.com>
2025-02-11 17:36:40 +0900

    [Feat] Coordinator protocol 구성

---
+++ DailyTyping/DailyTyping/Sources/Coordinator/Coordinator.swift
@@ -0,0 +1,20 @@
+//
+//  Coordinator.swift
+//  DailyTyping
+//
+//  Created by 조유진 on 2/11/25.
+//
+
+import UIKit
+
+protocol Coordinator: AnyObject {
+    var navigationController: UINavigationController { get }
+    var parentCoordinator: Coordinator? { get set }
+    var childCoordinators: [Coordinator] { get set }
+}
+
+extension Coordinator {
+    func removeChildCoordinator(child: Coordinator) {
+        childCoordinators.removeAll { $0 === child }
+    }
+}
