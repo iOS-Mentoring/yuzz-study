71dad90a75d7a302cbf3780f868697c060ba9207
yuzzin <ydbwlsjki60@gmail.com>
2025-02-11 17:43:58 +0900

    [Feat] AppCoordinator 구성

---
+++ DailyTyping/DailyTyping/Sources/Coordinator/AppCoordinator.swift
@@ -0,0 +1,26 @@
+//
+//  AppCoordinator.swift
+//  DailyTyping
+//
+//  Created by 조유진 on 2/11/25.
+//
+
+import UIKit
+
+final class AppCoordinator: Coordinator {
+    let navigationController: UINavigationController
+    weak var parentCoordinator: Coordinator?
+    var childCoordinators: [Coordinator] = []
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let mainCoordinator = MainCoordinator(navigationController: navigationController)
+        
+        mainCoordinator.parentCoordinator = self
+        childCoordinators.append(mainCoordinator)
+        mainCoordinator.start()
+    }
+}
