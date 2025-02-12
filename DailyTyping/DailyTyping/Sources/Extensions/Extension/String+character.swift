d994f4c9d74adc86803169df4b1c350b99cbb4aa
yuzzin <ydbwlsjki60@gmail.com>
2025-02-12 15:53:59 +0900

    [Feat] 입력한 characters 와 placeholder의 characters 일치 개수 로직 구현

---
+++ DailyTyping/DailyTyping/Sources/Extensions/Extension/String+character.swift
@@ -0,0 +1,36 @@
+//
+//  String+character.swift
+//  DailyTyping
+//
+//  Created by 조유진 on 2/12/25.
+//
+
+import Foundation
+
+extension String {
+    func getMatchHangulCharacterCount() -> Int {
+        var inputCharList = self.decomposedStringWithCanonicalMapping
+            .unicodeScalars
+            .map { scalar -> Character in
+                return Character(scalar)
+            }
+        
+        inputCharList.removeFirst()
+        
+        let placeholderCharList = TypingLabelText.typingValue.rawValue.decomposedStringWithCanonicalMapping
+            .unicodeScalars
+            .map { scalar -> Character in
+                return Character(scalar)
+            }
+        
+        var matchCount = 0
+        
+        for i in 0..<placeholderCharList.count {
+            if let inputCharacter = inputCharList[safe: i], let placeholderCharacter = placeholderCharList[safe: i], inputCharacter == placeholderCharacter {
+                matchCount += 1
+            }
+        }
+        
+        return matchCount
+    }
+}
