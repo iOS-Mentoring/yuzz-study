733ef96b3195123cf37b35bc27c29ebf732b25d0
yuzzin <ydbwlsjki60@gmail.com>
2025-02-12 09:29:18 +0900

    [Feat] LinkWebCoordinator 구성

---
+++ DailyTyping/DailyTyping/Sources/Coordinator/LinkWebCoordinator.swift
@@ -0,0 +1,30 @@
+//
+//  LinkWebCoordinator.swift
+//  DailyTyping
+//
+//  Created by 조유진 on 2/12/25.
+//
+
+import UIKit
+
+final class LinkWebCoordinator: Coordinator {
+    let navigationController: UINavigationController
+    weak var parentCoordinator: Coordinator?
+    var childCoordinators: [Coordinator] = []
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let viewModel = LinkWebViewModel()
+        let linkWebVC = LinkWebViewController(viewModel: viewModel)
+        linkWebVC.coordinator = self
+        navigationController.pushViewController(linkWebVC, animated: true)
+    }
+    
+    func removeCoordinator() {
+        print(#function)
+        parentCoordinator?.removeChildCoordinator(child: self)
+    }
+}
