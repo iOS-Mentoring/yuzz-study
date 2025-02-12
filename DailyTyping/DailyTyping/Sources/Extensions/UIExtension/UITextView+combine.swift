cb1baeec49bec57504979373f855331b6f9a717b
yuzzin <ydbwlsjki60@gmail.com>
2025-02-12 11:59:39 +0900

    [Feat] UITextView+combine 구성

---
+++ DailyTyping/DailyTyping/Sources/Extensions/UIExtension/UITextView+combine.swift
@@ -0,0 +1,22 @@
+//
+//  UITextView+combine.swift
+//  DailyTyping
+//
+//  Created by 조유진 on 2/12/25.
+//
+
+import UIKit
+import Combine
+
+extension UITextView {
+
+    var textPublisher: AnyPublisher<String, Never> {
+        NotificationCenter.default.publisher(
+            for: UITextView.textDidChangeNotification,
+            object: self
+        )
+        .compactMap { ($0.object as? UITextView)?.text }
+        .eraseToAnyPublisher()
+    }
+
+}
