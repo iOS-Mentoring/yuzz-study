3a2960764779f64b4496882f1d5d2329c8aaeb5c
yuzzin <ydbwlsjki60@gmail.com>
2025-02-14 10:57:37 +0900

    [Feat] TypingCompletedView 구성

---
+++ DailyTyping/DailyTyping/Sources/Scenes/TypingCompleted/View/TypingCompletedView.swift
@@ -0,0 +1,20 @@
+//
+//  TypingCompletedView.swift
+//  DailyTyping
+//
+//  Created by 조유진 on 2/14/25.
+//
+
+import UIKit
+
+final class TypingCompletedView: BaseView {
+    
+    
+    override func configureLayout() {
+        
+    }
+    
+    override func configureView() {
+        
+    }
+}
