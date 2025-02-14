cb8f2df2c926b87fffa2b89e09bb422e335d1e35
yuzzin <ydbwlsjki60@gmail.com>
2025-02-14 10:58:24 +0900

    [Feat] TypingCompletedViewModel 구성

---
+++ DailyTyping/DailyTyping/Sources/Scenes/TypingCompleted/ViewModel/TypingCompletedViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  TypingCompletedViewModel.swift
+//  DailyTyping
+//
+//  Created by 조유진 on 2/14/25.
+//
+
+import Combine
+
+final class TypingCompletedViewModel: ViewModelType {
+    private var cancellables = Set<AnyCancellable>()
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        
+        return Output()
+    }
+}
